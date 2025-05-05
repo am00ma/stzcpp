52144a85d20dce3011d97f268038fa707bb7c8bb
manyids2 <arun.mukundan@gmail.com>
2025-05-05 12:19:47 +0200

    RFX: More readable examples

--- include/arena.h
+++ include/arena.h
@@ -14,40 +14,29 @@ typedef enum {
     DEFAULTS = 0x4,
 } ArenaFlags;
 
-class Arena {
-  private:
+typedef struct Arena {
     char* beg = 0;
     char* end = 0;
     isize cap = 0;
 
-  public:
     Arena(isize cap);
     ~Arena();
 
     char* Alloc(isize objsize, isize align, isize count, b32 flags);
 
-    template <typename T> T* Make(isize count = 1, b32 flags = 0)
+    template <typename T, typename... A> T* Make(isize count = 1, b32 flags = 0, A... args)
     {
         T* r = (T*)Alloc(sizeof(T), alignof(T), count, flags);
 
-        // BUG:: Does not work,
-        //
-        //   include/arena.h:34:35: error: no matching function for call to ‘operator new(sizetype, void*)’
-        //      34 |             RANGE(i, count) { (T*)new ((void*)&r[i]) T; }
-        //         |                                   ^~~~~~~~~~~~~~~~~~~~
-        //   <built-in>: note: candidate: ‘void* operator new(long unsigned int)’
-        //   <built-in>: note:   candidate expects 1 argument, 2 provided
-        //   <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_val_t)’
-        //   <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::align_val_t’
-        //
-        // Allows for zero / default init
+        // Allows for zero / default init NOTE: needs `#include <new>`
+        // https://en.cppreference.com/w/cpp/language/new#Placement_new
         if (flags & DEFAULTS)
         {
-            RANGE(i, count) { new ((void*)&r[i]) T{}; } // https://en.cppreference.com/w/cpp/language/new#Placement_new
+            RANGE(i, count) { new ((void*)&r[i]) T(args...); }
         }
 
         return r;
     }
 
     void Print(const char* label);
-};
+} Arena;
--- test/test_std.cpp
+++ test/test_std.cpp
@@ -1,4 +1,5 @@
 #include "arena.h"
+#include "log.h"
 #include "range.h"
 
 #include <cstdio> // printf
@@ -6,35 +7,54 @@
 typedef struct Item {
     i32 a = 4;
     i32 b = 8;
-
 } Item;
 
 int main(void)
 {
-    Arena a = Arena(1024);
-
+    // ------------------------------------
+    Arena a = Arena(128); // 128 bytes
     a.Print("Initial");
 
+    // ------------------------------------
+    title("\nSingle element");
+
     i32* x = a.Make<i32>();
-    x[0]   = 0;
+
     printf("%d: %d\n", 0, x[0]);
     a.Print("Alloc: 1 i32");
 
-    RANGE(j, 10)
-    {
-        i32* z = a.Make<i32>(10, NOZERO);
-        RANGE(i, 10) { printf("%d ", z[i]); }
-        printf("\n");
-    }
+    // ------------------------------------
+    title("\nArray of elements (zeroed by default)");
 
-    i32* y = a.Make<i32>(10);
-    RANGE(i, 10) { y[i] = i; }
-    RANGE(i, 10) { printf("%ld: %d\n", i, y[i]); }
-    a.Print("Alloc: 10 i32");
+    i32* y = a.Make<i32>(3);
 
-    Item* q = a.Make<Item>(10, DEFAULTS);
-    RANGE(i, 10) { printf("%ld: %d, %d\n", i, q[i].a, q[i].b); }
-    a.Print("Alloc: 10 Items");
+    RANGE(i, 3) { printf("%ld: %d\n", i, y[i]); }
+    a.Print("Alloc: 3 i32");
 
+    // ------------------------------------
+    title("\nElements zeroed");
+
+    Item* qz = a.Make<Item>(3); // Have to get order of new correct
+
+    RANGE(i, 3) { printf("%ld: %d, %d\n", i, qz[i].a, qz[i].b); }
+    a.Print("Zeroed: 3 Items");
+
+    // ------------------------------------
+    title("\nElements with defaults");
+
+    Item* qd = a.Make<Item>(3, DEFAULTS); // Have to get order of new correct
+
+    RANGE(i, 3) { printf("%ld: %d, %d\n", i, qd[i].a, qd[i].b); }
+    a.Print("Defaults: 3 Items");
+
+    // ------------------------------------
+    title("\nElements with default args");
+
+    Item* qa = a.Make<Item>(3, DEFAULTS, 3, 5); // Have to get order of new correct
+
+    RANGE(i, 3) { printf("%ld: %d, %d\n", i, qa[i].a, qa[i].b); }
+    a.Print("Args: 3 Items");
+
+    // ------------------------------------
     return 0;
 }

287729137166b840d7f46308ebdd9f8aefa54f4a
manyids2 <arun.mukundan@gmail.com>
2025-05-05 16:30:50 +0200

    YES: Buf is working

--- test/test_buf.cpp
+++ test/test_buf.cpp
@@ -8,72 +8,72 @@
 
 constexpr isize PERM_CAPACITY = 128;
 
-// Str parse_and_format(Str src, Arena temp, Arena* perm)
-// {
-//     // Reserve PERM_CAPACITY so str_fmt can use rest
-//     Buf tmp = buf_new(PERM_CAPACITY, &temp);
-//
-//     // Copy source string ( can only be done before using temp_loop )
-//     buf_join(&tmp, src);
-//
-//     // Parse and append to tmp
-//     FOR(src)
-//     {
-//         if (src.buf[i] == 'l')
-//         {
-//             // Temp arena for this loop
-//             Arena temp_loop = temp;
-//
-//             // Format and append ( uses STR_MAXLEN = 1024 )
-//             Str found = str_fmt(&temp_loop, "Found l: %d\n", i);
-//
-//             /*
-//              * temp     :: 0x7fff192c7a70 (beg: 0x56aec34d93b0, end: 0x56aec34d97b0)
-//              *   left: 1024, used: 128, cap: 1152
-//              * temp_loop:: 0x7fff192c7a70 (beg: 0x56aec34d93bc, end: 0x56aec34d97b0)
-//              *   left: 1012, used: 140, cap: 1152
-//              *
-//              * 140 - 128 = 12 extra for `Found ... <10`
-//              * 141 - 128 = 13 extra for `Found ... >=10`
-//              */
-//
-//             // Store
-//             buf_join(&tmp, found); // copies memory
-//
-//             printf("\n---  %d :  ---\n", i);
-//             arena_print("temp     :", &temp);
-//             arena_print("temp_loop:", &temp_loop);
-//         }
-//     }
-//
-//     printf("\n-------------\n");
-//
-//     // Finalize temp arena ( Not neccessary, but frees STR_MAXLEN stuff )
-//     Str out = buf_final(&tmp, &temp);
-//     arena_print("temp     :", &temp);
-//
-//     // Store in permanent arena
-//     Str dst = str_copy(out, perm);
-//
-//     return dst;
-// }
-//
-// int main()
-// {
-//     Arena perm = arena_new(PERM_CAPACITY);
-//
-//     // Allocate just enough temp arena
-//     Arena temp = arena_new(STR_MAXLEN + PERM_CAPACITY);
-//
-//     Str src = Str("hello hi, alles good?\n");
-//
-//     Str dst = parse_and_format(src, temp, &perm);
-//     printf("%.*s", pstr(dst));
-//
-//     arena_print("After parse_and_format: perm:", &perm);
-//     arena_print("After parse_and_format: temp:", &temp);
-//
-//     return 0;
-// }
+Str parse_and_format(Str src, Arena temp, Arena* perm)
+{
+    // Reserve PERM_CAPACITY so str_fmt can use rest
+    Buf tmp = Buf(&temp, PERM_CAPACITY);
 
-int main() { return 0; }
+    // Copy source string ( can only be done before using temp_loop )
+    tmp.Join(src);
+
+    // Parse and append to tmp
+    RANGE(i, src.len)
+    {
+        if (src.buf[i] == 'l')
+        {
+            // Temp arena for this loop
+            Arena temp_loop = temp;
+
+            // Format and append ( uses STR_MAXLEN = 1024 )
+            Str found = Str(&temp_loop, 32, "Found l: %d\n", i);
+
+            /*
+             * temp     :: 0x7fff192c7a70 (beg: 0x56aec34d93b0, end: 0x56aec34d97b0)
+             *   left: 1024, used: 128, cap: 1152
+             * temp_loop:: 0x7fff192c7a70 (beg: 0x56aec34d93bc, end: 0x56aec34d97b0)
+             *   left: 1012, used: 140, cap: 1152
+             *
+             * 140 - 128 = 12 extra for `Found ... <10`
+             * 141 - 128 = 13 extra for `Found ... >=10`
+             */
+
+            // Store
+            tmp.Join(found); // copies memory
+
+            printf("\n--- found l >> i = %ld ---\n", i);
+
+            temp.Print("temp     :");
+            temp_loop.Print("temp_loop:");
+        }
+    }
+
+    printf("\n-------------\n");
+
+    // Finalize temp arena ( Not neccessary, but frees STR_MAXLEN stuff )
+    Str out = tmp.Final(&temp);
+    temp.Print("temp     :");
+
+    // Copy to permanent arena
+    Str dst = out.Copy(perm);
+
+    return dst;
+}
+
+int main()
+{
+    // ------------------------------------
+    Arena perm = Arena(1024); // 1KB
+    perm.Print("Initial(perm)");
+
+    Arena temp = Arena(1024); // 1KB
+    temp.Print("Initial(temp)");
+
+    Str src = "hello hi, alles good?\n";
+    Str dst = parse_and_format(src, temp, &perm);
+    printf("%.*s", pstr(dst));
+
+    perm.Print("After parse_and_format(perm):");
+    temp.Print("After parse_and_format(temp):");
+
+    return 0;
+}

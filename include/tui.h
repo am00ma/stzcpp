6258f33d18399d6a430cb2f5aed4655194adb5b0
manyids2 <arun.mukundan@gmail.com>
2025-05-06 21:11:33 +0200

    XXX: tui cant be fit into setup, handle, render; wierd behaviour due to lambdas?

--- include/tui.h
+++ include/tui.h
@@ -2,9 +2,10 @@
 
 #include "str.h"
 
-#include <ftxui/component/component.hpp>          // for Menu
-#include <ftxui/component/component_options.hpp>  // for MenuOption
-#include <ftxui/component/screen_interactive.hpp> // for ScreenInteractive
+#include <ftxui/component/component.hpp>          // Menu
+#include <ftxui/component/component_options.hpp>  // MenuOption
+#include <ftxui/component/screen_interactive.hpp> // ScreenInteractive
+#include <ftxui/dom/elements.hpp>                 // operator|, text, separator, vbox, etc.
 
 // Placeholder
 constexpr Str hello_hi = "{\"hello\": \"hi\", \"a\": [1, 2, 3, 4]}";
--- test/test_ftxui.cpp
+++ test/test_ftxui.cpp
@@ -1,11 +1,4 @@
-// Copyright 2020 Arthur Sonzogni. All rights reserved.
-// Use of this source code is governed by the MIT license that can be found in
-// the LICENSE file.
-
-#include <functional> // for function
 #include <iostream> // for basic_ostream::operator<<, operator<<, endl, basic_ostream, basic_ostream<>::__ostream_type, cout, ostream
-#include <string> // for string, basic_string, allocator
-#include <vector> // for vector
 
 #include "tui.h"
 

f52af162016672e3afd60fd54f3da9064ca20c03
manyids2 <arun.mukundan@gmail.com>
2025-07-26 18:11:27 +0200

    YES: Acceptable form for doctest

--- tests/doctest.h
+++ tests/doctest.h
@@ -2,9 +2,10 @@
 
 #include "log.h"
 
-// TODO: Can also collect cases in map, so we can properly display at end
+// TODO: Can remormat to for loop as well
 #define TEST_SUITE(label)                                                                                              \
     const char* TEST_TITLE          = label;                                                                           \
+    int         TEST_INDEX          = 0;                                                                               \
     int         TEST_CASES          = 0;                                                                               \
     int         TEST_CHECKS         = 0;                                                                               \
     int         TEST_SUCCESS_CASES  = 0;                                                                               \
@@ -12,14 +13,20 @@
     title("%s", TEST_TITLE);                                                                                           \
     pretty(COLOR_ITALIC_YELLOW, "  %3s  %-50s %8s  .  %8s ", "", "Case", "Correct", "Total");
 
-// Not sure why I need do while
+// Putting statements at end of for loop makes them execute after scope
+
+// clang-format off
 #define TEST_CASE(label)                                                                                               \
-    do {                                                                                                               \
-        TEST_CASES++;                                                                                                  \
-    } while ((debug("  %3d. %-50s %8d  .  %8d ", TEST_CASES, label, TEST_SUCCESS_CHECKS, TEST_CHECKS),                 \
-              (TEST_SUCCESS_CASES += (TEST_SUCCESS_CHECKS == TEST_CHECKS)),                                            \
-              ((TEST_CHECKS = 0), (TEST_SUCCESS_CHECKS = 0))) &&                                                       \
-             0);
+    for (TEST_INDEX = 0; TEST_INDEX < 1;                                                                               \
+            (                                                                                                          \
+            pretty(COLOR_RESET, "  %3d. %-50s %8d  .  %8d ", TEST_CASES+1, label, TEST_SUCCESS_CHECKS, TEST_CHECKS),   \
+            TEST_SUCCESS_CASES += (TEST_SUCCESS_CHECKS == TEST_CHECKS),                                                \
+            TEST_CHECKS = 0, TEST_SUCCESS_CHECKS = 0,                                                                  \
+            TEST_INDEX++,                                                                                              \
+            TEST_CASES++                                                                                               \
+            )                                                                                                          \
+        )
+// clang-format on
 
 // https://nullprogram.com/blog/2022/06/26/
 #define ASSERT(cond)                                                                                                   \
@@ -38,4 +45,4 @@
     } while (0);
 
 // Print test results
-#define TEST_RESULTS(...) pretty(COLOR_ITALIC_GREEN, " ✔ %3d / %3d cases", TEST_SUCCESS_CASES, TEST_CASES);
+#define TEST_RESULTS(...) pretty(COLOR_ITALIC_GREEN, "    ✔ %3d / %3d cases", TEST_SUCCESS_CASES, TEST_CASES);

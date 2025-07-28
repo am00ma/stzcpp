74fb686ab10f05cd93aba0a7c15e5d4031520036
manyids2 <arun.mukundan@gmail.com>
2025-07-28 16:04:29 +0200

    RFX: Hard moving stuff to old

--- include/util/templates/sql/create.h
+++
@@ -1,9 +0,0 @@
-/* Create SQL statement */
-constexpr char SQL_CREATE_TABLE[] = "CREATE TABLE COMPANY(             \n"
-                                    "  ID INT PRIMARY KEY     NOT NULL,\n"
-                                    "  NAME           TEXT    NOT NULL,\n"
-                                    "  AGE            INT     NOT NULL,\n"
-                                    "  ADDRESS        CHAR(50),        \n"
-                                    "  SALARY         REAL,            \n"
-                                    "  \"IMAGE BLOB\"          BLOB             \n"
-                                    ");                                \n";
